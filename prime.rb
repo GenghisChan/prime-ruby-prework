1e31550a978861695be51149882858bffe43c42e
Christofer Chan <devchumbing@gmail.com>
2018-06-15 02:37:28 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,13 +1,9 @@
-# Add  code here!
-
 def prime?(num)
-  x = 2
-  while(x <= num / 2)
-    if(num % x == 0)
-      false
-    break
+  if num < 0 or num == 0 or num == 1
+    return false
+  else
+    (2..num-1).to_a.all? do |possible_factor|
+      num % possible_factor != 0
+    end
   end
-x += 1
-end
-
 end
