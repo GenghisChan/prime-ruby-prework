d2f931660997afcd4044580c93a7ae4f7e0392a5
Christofer Chan <devchumbing@gmail.com>
2018-06-15 02:33:30 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,14 +1,12 @@
 # Add  code here!
-prime_num = [1, 2, 3, 5, 7, 11]
 
 def prime?(num)
-  x = 2 
+  x = 2
   while(x <= num / 2)
     if(num % x == 0)
       false
     break
   end
-      
+
 
 end
-  
